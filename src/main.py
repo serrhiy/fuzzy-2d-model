f6cd2de76f32467f1b9edba6dd57cf957c04a75b
Serhii Lytvynenko <serrheylitvinenko@gmail.com>
2025-09-26 01:15:50 +0300

    Implement basich plots

--- src/main.py
+++ src/main.py
@@ -4,6 +4,8 @@ from matplotlib import pyplot
 from matplotlib.axes import Axes
 from matplotlib.figure import Figure
 
+from collections.abc import Iterable
+
 
 START_X = 0
 START_Y = 20
@@ -32,15 +34,36 @@ def get_original_values() -> tuple[numpy.ndarray, numpy.ndarray, numpy.ndarray]:
 
 def get_trimf(
     x: numpy.ndarray, y: numpy.ndarray, z: numpy.ndarray
-) -> tuple[list[numpy.ndarray], list[numpy.ndarray], list[numpy.ndarray]]:
+) -> tuple[
+    Iterable[numpy.ndarray], Iterable[numpy.ndarray], Iterable[numpy.ndarray]
+]:
 
     x_means = numpy.linspace(min(x), max(x), INTERVALS_INPUTS_NUMBER)
     y_means = numpy.linspace(min(y), max(y), INTERVALS_INPUTS_NUMBER)
     z_means = numpy.linspace(min(z), max(z), INTERVALS_OUTPUTS_NUMBER)
 
-    x_trimf = (skfuzzy.trimf(x, [x_means[i] - 3, x_means[i], x_means[i] + 3]) for i in range(len(x_means)))
-    y_trimf = (skfuzzy.trimf(y, [y_means[i] - 3, y_means[i], y_means[i] + 3]) for i in range(len(y_means)))
-    z_trimf = (skfuzzy.trimf(z, [z_means[i] - 3, z_means[i], z_means[i] + 3]) for i in range(len(z_means)))
+    x_delta = (max(x) - min(x)) * 0.15
+    y_delta = (max(y) - min(y)) * 0.15
+    z_delta = (max(z) - min(z)) * 0.15
+
+    x_trimf = (
+        skfuzzy.trimf(x, [x_means[i] - x_delta, x_means[i], x_means[i] + x_delta])
+        for i in range(len(x_means))
+    )
+    y_trimf = (
+        skfuzzy.trimf(
+            numpy.linspace(min(y), max(y), POINTS_NUMBER),
+            [y_means[i] - y_delta, y_means[i], y_means[i] + y_delta],
+        )
+        for i in range(len(y_means))
+    )
+    z_trimf = (
+        skfuzzy.trimf(
+            numpy.linspace(min(z), max(z), POINTS_NUMBER),
+            [z_means[i] - z_delta, z_means[i], z_means[i] + z_delta],
+        )
+        for i in range(len(z_means))
+    )
 
     return x_trimf, y_trimf, z_trimf
 
@@ -64,6 +87,16 @@ def main():
     for x_trimf in x_trimfs:
         xt_axes.plot(x, x_trimf)
     xt_axes.set_title("x trimf")
+
+    for y_trimf in y_trimfs:
+        x_numbers = numpy.linspace(min(y), max(y), len(y_trimf))
+        yt_axes.plot(x_numbers, y_trimf)
+    yt_axes.set_title("y trimf")
+
+    for z_trimf in z_trimfs:
+        x_numbers = numpy.linspace(min(z), max(z), len(z_trimf))
+        zt_axes.plot(x_numbers, z_trimf)
+    zt_axes.set_title("z trimf")
 
     pyplot.show()
 
