cdcbc12ba79636d1b990f4482351f748af6f2fd9
Serhii Lytvynenko <serrheylitvinenko@gmail.com>
2025-09-28 21:29:00 +0300

    Add example with trapmf

--- src/main.py
+++ src/main.py
@@ -59,12 +59,140 @@ def get_best_functions_index_gaussian(value, means, sigma):
     return index
 
 
+def get_best_functions_index_trapmf(value, means, x_delta, k):
+    best_value = float("-inf")
+    index = -1
+    for i, mean in enumerate(means):
+        trimf = skfuzzy.trapmf(
+            numpy.array([value]),
+            [mean - x_delta, mean - x_delta / 2, mean + x_delta / 2, mean + x_delta],
+        )
+        if trimf > best_value:
+            best_value = trimf
+            index = i
+    return index
+
+
 def MSE(real: numpy.ndarray, model: numpy.ndarray) -> float:
     return numpy.mean((real - model) ** 2)
 
 
 def MAE(real: numpy.ndarray, model: numpy.ndarray) -> float:
     return numpy.mean(numpy.abs(real - model))
+
+
+def trapmf_model(x, y, z):
+    window2, xt_axes, yt_axes, zt_axes = get_plots(1, 3)
+    window2.set_size_inches(from_pixels_to_inches(WINDOW_SIZE, window2))
+    window2.canvas.manager.set_window_title("trapmf functions")
+
+    window3, zo_axes = get_plots(1, 1)
+    window3.set_size_inches(from_pixels_to_inches(WINDOW_SIZE, window3))
+    window3.canvas.manager.set_window_title("Z output")
+
+    x_means = numpy.linspace(min(x), max(x), INTERVALS_INPUTS_NUMBER)
+    y_means = numpy.linspace(min(y), max(y), INTERVALS_INPUTS_NUMBER)
+    z_means = numpy.linspace(min(z), max(z), INTERVALS_OUTPUTS_NUMBER)
+
+    x_delta = (max(x) - min(x)) * 0.15
+    y_delta = (max(y) - min(y)) * 0.15
+    z_delta = (max(z) - min(z)) * 0.15
+
+    x_trumfs = (
+        skfuzzy.trapmf(
+            x,
+            [
+                x_mean - x_delta,
+                x_mean - x_delta / 2,
+                x_mean + x_delta / 2,
+                x_mean + x_delta,
+            ],
+        )
+        for x_mean in x_means
+    )
+    y_trumfs = (
+        skfuzzy.trapmf(
+            numpy.linspace(min(y), max(y), POINTS_NUMBER),
+            [
+                y_mean - y_delta,
+                y_mean - y_delta / 2,
+                y_mean + y_delta / 2,
+                y_mean + y_delta,
+            ],
+        )
+        for y_mean in y_means
+    )
+    z_trumfs = (
+        skfuzzy.trapmf(
+            numpy.linspace(min(z), max(z), POINTS_NUMBER),
+            [
+                z_mean - z_delta,
+                z_mean - z_delta / 2,
+                z_mean + z_delta / 2,
+                z_mean + z_delta,
+            ],
+        )
+        for z_mean in z_means
+    )
+
+    for x_trumf in x_trumfs:
+        xt_axes.plot(x, x_trumf)
+    xt_axes.set_title("x gaussian")
+
+    for y_trumf in y_trumfs:
+        x_numbers = numpy.linspace(min(y), max(y), len(y_trumf))
+        yt_axes.plot(x_numbers, y_trumf)
+    yt_axes.set_title("y gaussian")
+
+    for z_trumf in z_trumfs:
+        x_numbers = numpy.linspace(min(z), max(z), len(z_trumf))
+        zt_axes.plot(x_numbers, z_trumf)
+    zt_axes.set_title("z gaussian")
+
+    print("Table of values")
+    table = [["x/y"] + [str(x) for x in x_means]]
+    for y_mean in y_means:
+        row = [round(y_mean, 2)]
+        for x_mean in x_means:
+            z_value = calculate_z(x_mean, y_mean)
+            row.append(round(z_value, 2))
+        table.append(row)
+    print(tabulate(table, tablefmt="grid"))
+
+    print("Table of functions name")
+    table = [["x/y"] + ["mx" + str(i + 1) for i in range(len(x_means))]]
+    rules = {}
+    for y_index, y_mean in enumerate(y_means):
+        row = ["my" + str(y_index + 1)]
+        for x_index, x_mean in enumerate(x_means):
+            z_value = calculate_z(x_mean, y_mean)
+            best_functions_index = get_best_functions_index_trapmf(z_value, z_means, z_delta, 2)
+            row.append("mf" + str(best_functions_index + 1))
+            rules[(x_index, y_index)] = best_functions_index
+        table.append(row)
+    print(tabulate(table, tablefmt="grid"))
+
+    for (x_number, y_number), z_number in rules.items():
+        print(
+            f"if (x is mx{x_number + 1}) and (y is my{y_number + 1}) then (z is mf{z_number + 1})"
+        )
+    
+    z_output = []
+    for x_value in x:
+        y_value = calculate_y(x_value)
+        best_x = get_best_functions_index_trapmf(x_value, x_means, x_delta, 2)
+        best_y = get_best_functions_index_trapmf(y_value, y_means, y_delta, 2)
+        best_z = rules[(best_x, best_y)]
+        z_output.append(z_means[best_z])
+    zo_axes.plot(x, z_output, label="Model")
+    zo_axes.plot(x, z, label="True")
+    zo_axes.legend()
+
+    mean_squared_error = MSE(z, z_output)
+    mean_absolute_error = MAE(z, z_output)
+
+    print(f"Mean Squared Error {mean_squared_error}")
+    print(f"Mean Absolute Error {mean_absolute_error}")
 
 
 def gaussian_model(x, y, z):
@@ -267,7 +395,7 @@ def main():
     xy_axes.set_title("y")
     xz_axes.set_title("z")
 
-    trimf_model(x, y, z)
+    gaussian_model(x, y, z)
 
     pyplot.show()
 
