7b81bd73914c8e6d892e2f73bd37dc7b466e897c
Serhii Lytvynenko <serrheylitvinenko@gmail.com>
2025-09-28 22:06:38 +0300

    Add test with diagonals

--- src/main.py
+++ src/main.py
@@ -6,6 +6,8 @@ from matplotlib.figure import Figure
 
 from tabulate import tabulate
 
+from sklearn.metrics import mean_squared_error, mean_absolute_error
+
 
 START_X = 0
 START_Y = 20
@@ -37,6 +39,39 @@ def from_pixels_to_inches(size: tuple[int, int], window: Figure) -> tuple[float,
     return (width / window.dpi, height / window.dpi)
 
 
+def display_distribution_functions(
+    x, y, z, x_distributions, y_distributions, z_distributions
+):
+    window, xg_axes, yg_axes, zg_axes = get_plots(1, 3)
+    window.set_size_inches(from_pixels_to_inches(WINDOW_SIZE, window))
+    window.canvas.manager.set_window_title("Distribution functions")
+
+    for x_distribution in x_distributions:
+        xg_axes.plot(x, x_distribution)
+    xg_axes.set_title("x distribution")
+
+    for y_distribution in y_distributions:
+        x_numbers = numpy.linspace(min(y), max(y), len(y_distribution))
+        yg_axes.plot(x_numbers, y_distribution)
+    yg_axes.set_title("y distribution")
+
+    for z_distribution in z_distributions:
+        x_numbers = numpy.linspace(min(z), max(z), len(z_distribution))
+        zg_axes.plot(x_numbers, z_distribution)
+    zg_axes.set_title("z distribution")
+
+
+def print_values(x_means, y_means):
+    print("Table of values")
+    table = [["x/y"] + [str(x) for x in x_means]]
+    for i in range(len(x_means)):
+        x_mean, y_mean = x_means[i], y_means[i]
+        z_value = calculate_z(x_mean, y_mean)
+        row = [round(y_mean, 2)] + [z_value] * len(x_means)
+        table.append(row)
+    print(tabulate(table, tablefmt="grid"))
+
+
 def get_best_functions_index_trimf(value, means, diff):
     best_value = float("-inf")
     index = -1
@@ -65,7 +100,7 @@ def get_best_functions_index_trapmf(value, means, x_delta, k):
     for i, mean in enumerate(means):
         trimf = skfuzzy.trapmf(
             numpy.array([value]),
-            [mean - x_delta, mean - x_delta / 2, mean + x_delta / 2, mean + x_delta],
+            [mean - x_delta, mean - x_delta / k, mean + x_delta / k, mean + x_delta],
         )
         if trimf > best_value:
             best_value = trimf
@@ -73,26 +108,72 @@ def get_best_functions_index_trapmf(value, means, x_delta, k):
     return index
 
 
-def MSE(real: numpy.ndarray, model: numpy.ndarray) -> float:
-    return numpy.mean((real - model) ** 2)
-
-
-def MAE(real: numpy.ndarray, model: numpy.ndarray) -> float:
-    return numpy.mean(numpy.abs(real - model))
-
-
-def trapmf_model(x, y, z):
-    window2, xt_axes, yt_axes, zt_axes = get_plots(1, 3)
-    window2.set_size_inches(from_pixels_to_inches(WINDOW_SIZE, window2))
-    window2.canvas.manager.set_window_title("trapmf functions")
-
+def diagonal_model(x, y, z, x_means, y_means, z_means):
     window3, zo_axes = get_plots(1, 1)
     window3.set_size_inches(from_pixels_to_inches(WINDOW_SIZE, window3))
     window3.canvas.manager.set_window_title("Z output")
 
-    x_means = numpy.linspace(min(x), max(x), INTERVALS_INPUTS_NUMBER)
-    y_means = numpy.linspace(min(y), max(y), INTERVALS_INPUTS_NUMBER)
-    z_means = numpy.linspace(min(z), max(z), INTERVALS_OUTPUTS_NUMBER)
+    x_sigma = (max(x) - min(x)) * 0.15
+    y_sigma = (max(y) - min(y)) * 0.15
+    z_sigma = (max(z) - min(z)) * 0.15
+
+    x_gaussians = (skfuzzy.gaussmf(x, x_mean, x_sigma) for x_mean in x_means)
+    y_gaussians = (
+        skfuzzy.gaussmf(numpy.linspace(min(y), max(y), POINTS_NUMBER), y_mean, y_sigma)
+        for y_mean in y_means
+    )
+    z_gaussians = (
+        skfuzzy.gaussmf(numpy.linspace(min(z), max(z), POINTS_NUMBER), z_mean, z_sigma)
+        for z_mean in z_means
+    )
+
+    display_distribution_functions(x, y, z, x_gaussians, y_gaussians, z_gaussians)
+
+    print_values(x_means, y_means)
+
+    print("Table of functions name")
+    table = [["x/y"] + ["mx" + str(i + 1) for i in range(len(x_means))]]
+    rules = {}
+    for i in range(len(x_means)):
+        x_mean, y_mean = x_means[i], y_means[i]
+        z_value = calculate_z(x_mean, y_mean)
+        best_functions_index = get_best_functions_index_gaussian(
+            z_value, z_means, z_sigma
+        )
+        row = ["my" + str(i + 1)] + ["mf" + str(best_functions_index + 1)] * len(
+            x_means
+        )
+        table.append(row)
+        for j in range(len(x_means)):
+            rules[(j, i)] = best_functions_index
+    print(tabulate(table, tablefmt="grid"))
+
+    for (x_number, y_number), z_number in rules.items():
+        print(
+            f"if (x is mx{x_number + 1}) and (y is my{y_number + 1}) then (z is mf{z_number + 1})"
+        )
+
+    z_output = []
+    for x_value in x:
+        y_value = calculate_y(x_value)
+        best_x = get_best_functions_index_gaussian(x_value, x_means, x_sigma)
+        best_y = get_best_functions_index_gaussian(y_value, y_means, y_sigma)
+        best_z = rules[(best_x, best_y)]
+        z_output.append(z_means[best_z])
+    zo_axes.plot(x, z_output, label="Model")
+    zo_axes.plot(x, z, label="True")
+    zo_axes.legend()
+
+    mse = mean_squared_error(z, z_output)
+    mae = mean_absolute_error(z, z_output)
+    print(f"Mean Squared Error {mse}")
+    print(f"Mean Absolute Error {mae}")
+
+
+def trapmf_model(x, y, z, x_means, y_means, z_means):
+    window3, zo_axes = get_plots(1, 1)
+    window3.set_size_inches(from_pixels_to_inches(WINDOW_SIZE, window3))
+    window3.canvas.manager.set_window_title("Z output")
 
     x_delta = (max(x) - min(x)) * 0.15
     y_delta = (max(y) - min(y)) * 0.15
@@ -135,29 +216,9 @@ def trapmf_model(x, y, z):
         for z_mean in z_means
     )
 
-    for x_trumf in x_trumfs:
-        xt_axes.plot(x, x_trumf)
-    xt_axes.set_title("x gaussian")
+    display_distribution_functions(x, y, z, x_trumfs, y_trumfs, z_trumfs)
 
-    for y_trumf in y_trumfs:
-        x_numbers = numpy.linspace(min(y), max(y), len(y_trumf))
-        yt_axes.plot(x_numbers, y_trumf)
-    yt_axes.set_title("y gaussian")
-
-    for z_trumf in z_trumfs:
-        x_numbers = numpy.linspace(min(z), max(z), len(z_trumf))
-        zt_axes.plot(x_numbers, z_trumf)
-    zt_axes.set_title("z gaussian")
-
-    print("Table of values")
-    table = [["x/y"] + [str(x) for x in x_means]]
-    for y_mean in y_means:
-        row = [round(y_mean, 2)]
-        for x_mean in x_means:
-            z_value = calculate_z(x_mean, y_mean)
-            row.append(round(z_value, 2))
-        table.append(row)
-    print(tabulate(table, tablefmt="grid"))
+    print_values(x_means, y_means)
 
     print("Table of functions name")
     table = [["x/y"] + ["mx" + str(i + 1) for i in range(len(x_means))]]
@@ -166,7 +227,9 @@ def trapmf_model(x, y, z):
         row = ["my" + str(y_index + 1)]
         for x_index, x_mean in enumerate(x_means):
             z_value = calculate_z(x_mean, y_mean)
-            best_functions_index = get_best_functions_index_trapmf(z_value, z_means, z_delta, 2)
+            best_functions_index = get_best_functions_index_trapmf(
+                z_value, z_means, z_delta, 2
+            )
             row.append("mf" + str(best_functions_index + 1))
             rules[(x_index, y_index)] = best_functions_index
         table.append(row)
@@ -176,7 +239,7 @@ def trapmf_model(x, y, z):
         print(
             f"if (x is mx{x_number + 1}) and (y is my{y_number + 1}) then (z is mf{z_number + 1})"
         )
-    
+
     z_output = []
     for x_value in x:
         y_value = calculate_y(x_value)
@@ -188,25 +251,16 @@ def trapmf_model(x, y, z):
     zo_axes.plot(x, z, label="True")
     zo_axes.legend()
 
-    mean_squared_error = MSE(z, z_output)
-    mean_absolute_error = MAE(z, z_output)
+    mse = mean_squared_error(z, z_output)
+    mae = mean_absolute_error(z, z_output)
+    print(f"Mean Squared Error {mse}")
+    print(f"Mean Absolute Error {mae}")
 
-    print(f"Mean Squared Error {mean_squared_error}")
-    print(f"Mean Absolute Error {mean_absolute_error}")
 
-
-def gaussian_model(x, y, z):
-    window2, xg_axes, yg_axes, zg_axes = get_plots(1, 3)
-    window2.set_size_inches(from_pixels_to_inches(WINDOW_SIZE, window2))
-    window2.canvas.manager.set_window_title("Gaussian functions")
-
+def gaussian_model(x, y, z, x_means, y_means, z_means):
     window3, zo_axes = get_plots(1, 1)
     window3.set_size_inches(from_pixels_to_inches(WINDOW_SIZE, window3))
     window3.canvas.manager.set_window_title("Z output")
-
-    x_means = numpy.linspace(min(x), max(x), INTERVALS_INPUTS_NUMBER)
-    y_means = numpy.linspace(min(y), max(y), INTERVALS_INPUTS_NUMBER)
-    z_means = numpy.linspace(min(z), max(z), INTERVALS_OUTPUTS_NUMBER)
 
     x_sigma = (max(x) - min(x)) * 0.15
     y_sigma = (max(y) - min(y)) * 0.15
@@ -222,29 +276,9 @@ def gaussian_model(x, y, z):
         for z_mean in z_means
     )
 
-    for x_gaussian in x_gaussians:
-        xg_axes.plot(x, x_gaussian)
-    xg_axes.set_title("x gaussian")
+    display_distribution_functions(x, y, z, x_gaussians, y_gaussians, z_gaussians)
 
-    for y_gaussian in y_gaussians:
-        x_numbers = numpy.linspace(min(y), max(y), len(y_gaussian))
-        yg_axes.plot(x_numbers, y_gaussian)
-    yg_axes.set_title("y gaussian")
-
-    for z_gaussian in z_gaussians:
-        x_numbers = numpy.linspace(min(z), max(z), len(z_gaussian))
-        zg_axes.plot(x_numbers, z_gaussian)
-    zg_axes.set_title("z gaussian")
-
-    print("Table of values")
-    table = [["x/y"] + [str(x) for x in x_means]]
-    for y_mean in y_means:
-        row = [round(y_mean, 2)]
-        for x_mean in x_means:
-            z_value = calculate_z(x_mean, y_mean)
-            row.append(round(z_value, 2))
-        table.append(row)
-    print(tabulate(table, tablefmt="grid"))
+    print_values(x_means, y_means)
 
     print("Table of functions name")
     table = [["x/y"] + ["mx" + str(i + 1) for i in range(len(x_means))]]
@@ -277,25 +311,16 @@ def gaussian_model(x, y, z):
     zo_axes.plot(x, z, label="True")
     zo_axes.legend()
 
-    mean_squared_error = MSE(z, z_output)
-    mean_absolute_error = MAE(z, z_output)
+    mse = mean_squared_error(z, z_output)
+    mae = mean_absolute_error(z, z_output)
+    print(f"Mean Squared Error {mse}")
+    print(f"Mean Absolute Error {mae}")
 
-    print(f"Mean Squared Error {mean_squared_error}")
-    print(f"Mean Absolute Error {mean_absolute_error}")
 
-
-def trimf_model(x, y, z):
-    window2, xt_axes, yt_axes, zt_axes = get_plots(1, 3)
-    window2.set_size_inches(from_pixels_to_inches(WINDOW_SIZE, window2))
-    window2.canvas.manager.set_window_title("Trimf functions")
-
+def trimf_model(x, y, z, x_means, y_means, z_means):
     window3, zo_axes = get_plots(1, 1)
     window3.set_size_inches(from_pixels_to_inches(WINDOW_SIZE, window3))
     window3.canvas.manager.set_window_title("Z output")
-
-    x_means = numpy.linspace(min(x), max(x), INTERVALS_INPUTS_NUMBER)
-    y_means = numpy.linspace(min(y), max(y), INTERVALS_INPUTS_NUMBER)
-    z_means = numpy.linspace(min(z), max(z), INTERVALS_OUTPUTS_NUMBER)
 
     x_delta = (max(x) - min(x)) * 0.15
     y_delta = (max(y) - min(y)) * 0.15
@@ -320,29 +345,9 @@ def trimf_model(x, y, z):
         for z_mean in z_means
     )
 
-    for x_trimf in x_trimfs:
-        xt_axes.plot(x, x_trimf)
-    xt_axes.set_title("x trimf")
+    display_distribution_functions(x, y, z, x_trimfs, y_trimfs, z_trimfs)
 
-    for y_trimf in y_trimfs:
-        x_numbers = numpy.linspace(min(y), max(y), len(y_trimf))
-        yt_axes.plot(x_numbers, y_trimf)
-    yt_axes.set_title("y trimf")
-
-    for z_trimf in z_trimfs:
-        x_numbers = numpy.linspace(min(z), max(z), len(z_trimf))
-        zt_axes.plot(x_numbers, z_trimf)
-    zt_axes.set_title("z trimf")
-
-    print("Table of values")
-    table = [["x/y"] + [str(x) for x in x_means]]
-    for y_mean in y_means:
-        row = [round(y_mean, 2)]
-        for x_mean in x_means:
-            z_value = calculate_z(x_mean, y_mean)
-            row.append(round(z_value, 2))
-        table.append(row)
-    print(tabulate(table, tablefmt="grid"))
+    print_values(x_means, y_means)
 
     print("Table of functions name")
     table = [["x/y"] + ["mx" + str(i + 1) for i in range(len(x_means))]]
@@ -375,17 +380,20 @@ def trimf_model(x, y, z):
     zo_axes.plot(x, z, label="True")
     zo_axes.legend()
 
-    mean_squared_error = MSE(z, z_output)
-    mean_absolute_error = MAE(z, z_output)
-
-    print(f"Mean Squared Error {mean_squared_error}")
-    print(f"Mean Absolute Error {mean_absolute_error}")
+    mse = mean_squared_error(z, z_output)
+    mae = mean_absolute_error(z, z_output)
+    print(f"Mean Squared Error {mse}")
+    print(f"Mean Absolute Error {mae}")
 
 
 def main():
     x = numpy.linspace(START_X, START_Y, POINTS_NUMBER)
     y = calculate_y(x)
     z = calculate_z(x, y)
+
+    x_means = numpy.linspace(min(x), max(x), INTERVALS_INPUTS_NUMBER)
+    y_means = numpy.linspace(min(y), max(y), INTERVALS_INPUTS_NUMBER)
+    z_means = numpy.linspace(min(z), max(z), INTERVALS_OUTPUTS_NUMBER)
 
     window, xy_axes, xz_axes = get_plots(1, 2)
     window.set_size_inches(from_pixels_to_inches(WINDOW_SIZE, window))
@@ -395,7 +403,7 @@ def main():
     xy_axes.set_title("y")
     xz_axes.set_title("z")
 
-    gaussian_model(x, y, z)
+    diagonal_model(x, y, z, x_means, y_means, z_means)
 
     pyplot.show()
 
